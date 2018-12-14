7df19a99d8fc5e30f32c7b81a186143adad51033
BT_1N3_33@BKTECH.LOCAL <BT_1N3_33@BKTECH.LOCAL>
2018-12-14 10:14:07 -0500

    Initial commit

---
+++ src/CSVUtilities.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+
+public class CSVUtilities {
+    ArrayList<String> CSVData;
+    int numColumns;
+
+    public CSVUtilities(File csv)
+    {
+        String fileName = "CSVGraphLab/Water_Consumption_In_The_New_York_City.csv";
+        File file = new File(fileName);
+    }
+
+    public List<String> getColumnHeaders()
+    {
+        return
+    }
+
+    public List<String> getDataString(int column)
+    {
+        return
+    }
+
+    public List<Integer> getDataInt(int column)
+    {
+        return
+    }
+
+    public List<Double> getDataDouble(int column)
+    {
+        return
+    }
+}
